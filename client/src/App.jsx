8cf2c6317aa4dc863c9ba35217d883e519656224
GDIV <gdavisiv@gmail.com>
2023-05-11 22:41:06 -0400

    Edit: Added Imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="head-text">React App</div>
+    <div>
+    <h1 className="head-text">ThreeJS</h1>
+    </div>
   )
 }
 
