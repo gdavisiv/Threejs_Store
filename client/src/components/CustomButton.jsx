7021567617e431489f707702c0eb6e5300553f45
GDIV <gdavisiv@gmail.com>
2023-05-19 16:14:26 -0400

    Edit: Con't Styling

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const generateStyle = (type) => {
+    if(type === 'filled') {
+      return {
+        backgroundColor='#000',
+        color: '#fff'
+      }
+    }
+  }
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-      style={generateStyle()}
+      style={generateStyle(type)}
     >
       {title}
     </button>
