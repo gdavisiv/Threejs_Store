474d8a108ee321c08001cfd5360def89d8a99813
GDIV <gdavisiv@gmail.com>
2023-05-19 16:10:29 -0400

    Edit: Adding in Custom Props and styles

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const CustomButton = () => {
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
   return (
     <button
-      className={'px-2'}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle()}
     >
       {title}
     </button>
