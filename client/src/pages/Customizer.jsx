dc0749ccf82d3fd0ac309190dfdce4c2cb2727ce
GDIV <gdavisiv@gmail.com>
2023-05-26 22:44:25 -0400

    Edit: Error Fixed: **Typo's**

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useSnapShot } from 'valtio';
+import { useSnapshot } from 'valtio';
 
 import config from '../config/config';
 import state from '../store';
@@ -12,7 +12,7 @@ import { fadeAnimation, slideAnimation } from  '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
-  const snap = useSnapShot(state);
+  const snap = useSnapshot(state);
   return (
     <AnimatePresence>
       {!snap.intro && (
