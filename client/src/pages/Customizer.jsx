4677d78a88ea36e2796933110a9e9625679b8fe6
GDIV <gdavisiv@gmail.com>
2023-05-27 23:54:27 -0400

    Edit: Customizing View

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -17,7 +17,16 @@ const Customizer = () => {
     <AnimatePresence>
       {!snap.intro && (
         <>
-          <motion.div>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min h-screen">
+              <div className="editortabs-container tabs">
+
+              </div>
+            </div>
             
           </motion.div>
         </>
