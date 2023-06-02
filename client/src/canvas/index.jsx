7f4aa80b274c6c53a6366364418713ededa0a653
GDIV <gdavisiv@gmail.com>
2023-06-02 16:53:39 -0400

    Revert "Edit: Displays 3D Shirt"
    This reverts commit 5f19414a0a1083a1dc9602154d75a64a06693328.

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,4 +1,4 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
@@ -7,18 +7,23 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
-      
+
       <CameraRig>
         <Backdrop />
-          <Center>
-            <Shirt />
-          </Center>
+        <Center>
+          <Shirt />
+        </Center>
       </CameraRig>
     </Canvas>
   )
 }
 
-export default CanvasModel;
+export default CanvasModel
