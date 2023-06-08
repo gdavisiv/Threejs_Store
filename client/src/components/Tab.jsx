06d937c73aa27c98a35bf7af695ef895ae2df3d3
GDIV <gdavisiv@gmail.com>
2023-06-08 13:20:29 -0400

    Edit: Added Icon Display on Menu Bars

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -6,14 +6,20 @@ import state from '../store';
 const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
 
+  const activeStyles = isFilterTab && isActiveTab 
+    ? { backgroundColor: snap.color, opacity: 0.5 }
+    : { backgroundColor: "transparent", opacity: 1}
+
   return (
     <div
       key={tab.name}
       className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
       onClick={handleClick}
-    
+      styles={activeStyles}
     >
-
+      <img 
+        src={tab.icon}
+      />
     </div>
   )
 }
