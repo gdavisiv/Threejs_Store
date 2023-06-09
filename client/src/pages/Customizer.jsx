e71b3df8e8760b4a1ccf10c520e0d5f0a287feab
GDIV <gdavisiv@gmail.com>
2023-06-09 15:15:19 -0400

    Edit: Creasting 'useStates' for tab functionality

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -16,9 +16,20 @@ import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../compone
 const Customizer = () => {
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+
   // Tab Button Content will Change depending on the activeTab
   const generateTabContent = () => {
-    
+
   }
 
   return (
