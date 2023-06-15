c14cb448a8ca37eb9be7e726870bc8c2dfc0a245
GDIV <gdavisiv@gmail.com>
2023-06-14 22:17:34 -0400

    Edit: Added Color Choice Functionality

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -5,9 +5,18 @@ import { useSnapshot } from 'valtio';
 import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => state.color = color.hex}
+      
+      />
+    </div>
   )
 }
 
-export default ColorPicker
+export default ColorPicker;
